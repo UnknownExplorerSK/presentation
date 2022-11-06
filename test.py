be4ed8268a3d2b6251d852b2362dd4d922eedb45
lkjg(*8w5 <krolssebastains@gmail.com>
2022-11-06 20:00:33 +0200

    ending slide

--- test.py
+++ test.py
@@ -28,6 +28,7 @@ while i > 0:
   #print(slide_number)
 
   while x == 0:
+    i -= 1
     slide_number_register = pr1.slide_layouts[0]
     slide_number = pr1.slides.add_slide(slide_number_register)
 
@@ -39,7 +40,6 @@ while i > 0:
     x = x+1
 
   else:
-
     slide_number_register = pr1.slide_layouts[1]
     slide_number = pr1.slides.add_slide(slide_number_register)
 
@@ -48,30 +48,20 @@ while i > 0:
     input1_ending = "slide"
     var = input('Title of slide number ' + str(x+1) + ':')
     title_number.text = var
-  
+    x = x+1
 
-  x = x+1
+    while i == 0:
+      slide_number_register = pr1.slide_layouts[0]
+      slide_number = pr1.slides.add_slide(slide_number_register)
+
+      title_number = slide_number.shapes.title
+      title_number.text = "Are there any questions?"
+
+      subtitle1 = slide_number.placeholders[1]
+      subtitle1.text = "Have a wonderful day!"
+      x = x+1
+      i -= 1
+  
 else:
   pr1.save("Testing.pptx")
-
-# for i in range(0,n):
-#     print(i)
-
-# slide1_register = pr1.slide_layouts[0]
-# slide0 = pr1.slides.add_slide(slide1_register)
-
-# title1 = slide0.shapes.title
-# var = input("Please enter the topic of the presentation: ")
-# title1.text = var
-
-# subtitle1 = slide0.placeholders[1]
-# var2 = input("Please enter who made the presentation:")
-# subtitle1.text = var2
-
-# slide2_register = pr1.slide_layouts[1]
-# slide2 = pr1.slides.add_slide(slide2_register)
-
-# title2 = slide2.shapes.title
-# title2.text = "Propoganda"
-
-# pr1.save("Testing.pptx")
+  print("Presentation done!")
